232259e183e8e248c985d365e73273b5cee6de06
noahpulsipher <31518741+noahpulsipher@users.noreply.github.com>
2018-01-25 13:35:54 -0700

    Added my name, 13-30, 31 to get rid of the comma at the end, an adress for the variable holding my name, and a cout with three "<<".

--- SampleProject/Controller/SampleController.cpp
+++ SampleProject/Controller/SampleController.cpp
@@ -1,10 +1,8 @@
-//
 //  SampleController.cpp
 //  SampleProject
 //
 //  Created by Pulsipher, Noah on 1/25/18.
 //  Copyright © 2018 CTEC. All rights reserved.
-//
 
 #include "SampleController.hpp"
 
@@ -17,21 +15,18 @@ Controller :: Controller()
 
 void Controller :: start()
 {
-//    for (int index = 0; index < 10; index ++)
-//    {
-//        cout << "This is the " << index + 1 << " time in the loop" << endl;
-//    }
+    string name = "Noah Pulsipher";
+    cout << name << endl;
     
-    string Words[2] = {"Hi!","Bye!"};
-    cout << Words[0] << endl;
-    
-    int const static arrayLength = 11;
-    int Numbers[arrayLength] = {0,1,2,3,4,5,6,7,8,9,10};
-    for (int num = 0; num < arrayLength; num ++)
+    for (int num = 13; num < 31; num ++)
     {
-        cout << Numbers[num] << endl;
+        cout << num << ",";
     }
+    cout << 31 << endl;
+
+    cout << &name << endl;
     
-   cout << Words[1] << endl;
+    cout << "Hello! " << "How are you today? " << "I am doing well." << endl;
+    
 }
 
