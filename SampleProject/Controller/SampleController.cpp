79d039983c576fb1812c81966f7f52a92ffa2b2f
noahpulsipher <31518741+noahpulsipher@users.noreply.github.com>
2018-01-25 11:52:01 -0700

    Created a controller group with a controller and a runner file.

---
+++ SampleProject/Controller/SampleController.cpp
@@ -0,0 +1,9 @@
+//
+//  SampleController.cpp
+//  SampleProject
+//
+//  Created by Pulsipher, Noah on 1/25/18.
+//  Copyright © 2018 CTEC. All rights reserved.
+//
+
+#include "SampleController.hpp"
