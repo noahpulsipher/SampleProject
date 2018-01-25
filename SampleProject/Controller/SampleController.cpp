01045ff610e977a734a1de86ef56871f9bdcf641
noahpulsipher <31518741+noahpulsipher@users.noreply.github.com>
2018-01-25 12:40:54 -0700

    Made an array of words and an array of numbers to practice.

--- SampleProject/Controller/SampleController.cpp
+++ SampleProject/Controller/SampleController.cpp
@@ -12,13 +12,26 @@ using namespace std;
 
 Controller :: Controller()
 {
-    
+
 }
 
 void Controller :: start()
 {
-    for (int index = 0; index < 10; index ++)
+//    for (int index = 0; index < 10; index ++)
+//    {
+//        cout << "This is the " << index + 1 << " time in the loop" << endl;
+//    }
+    
+    string Words[2] = {"Hi!","Bye!"};
+    cout << Words[0] << endl;
+    
+    int const static arrayLength = 11;
+    int Numbers[arrayLength] = {0,1,2,3,4,5,6,7,8,9,10};
+    for (int num = 0; num < arrayLength; num ++)
     {
-        cout << "This is the " << index + 1 << " time in the loop" << endl;
+        cout << Numbers[num] << endl;
     }
+    
+   cout << Words[1] << endl;
 }
+
